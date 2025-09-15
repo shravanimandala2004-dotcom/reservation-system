371bea43201d44f73d55c7a6926fbb8c77a48cec
Silva <Alvita.Silva@commscope.com>
2025-09-15 16:14:00 +0530

    Added flask route for reservation handling

--- app/routes/reservation_routes.py
+++ app/routes/reservation_routes.py
@@ -112,9 +112,10 @@ def reserve_page():
         controller = cursor.fetchone()
 
     cursor.execute("""
-        SELECT r.*, res.name AS resource_name, res.link
+        SELECT r.*, res.name AS resource_name, res.link, c.name AS controller_name
         FROM reservations r
-        JOIN resources res ON r.resource_id = res.id
+        LEFT JOIN resources res ON r.resource_id = res.id
+        LEFT JOIN controllers c ON r.controller_id = c.controller_id
         WHERE r.user_id = %s AND r.end_datetime >= NOW()
     """, (user_id,))
     active_reservations = cursor.fetchall()
@@ -132,6 +133,77 @@ def reserve_page():
         max_reservations=max_reservations,
         max_days=max_days,
         role=session.get('role'))
+
+@reservation_bp.route('/reserve_page', methods=['POST'])
+def handle_reservation():
+    if 'user_id' not in session:
+        return redirect(url_for('auth.index'))
+    
+    user_id=session['user_id']
+    conn=get_db_connection()
+    cursor=conn.cursor(dictionary=True)
+
+    # Check active reservations
+    max_reservations = get_setting('max_reservations', 2)
+    cursor.execute("""
+        SELECT count(res.controller_id) as count
+        FROM reservations res
+        WHERE res.user_id = %s AND res.end_datetime >= NOW()
+    """, (user_id,))
+    active = cursor.fetchall()
+    active_count = active[0]['count']
+    
+    resource_id = request.form['resource_id']
+    controller_id = request.form['controller_id']
+    if(resource_id):
+        cursor.execute("SELECT * FROM resources WHERE id = %s", (resource_id,))
+    resource = cursor.fetchone()
+
+    cursor.execute("SELECT * FROM controllers WHERE controller_id = %s", (controller_id,))
+    controller = cursor.fetchone()
+
+    print("resource:",resource,"controller:",controller)
+
+    start_dt = datetime.strptime(request.form['start_datetime'], '%Y-%m-%dT%H:%M')
+    end_dt = datetime.strptime(request.form['end_datetime'], '%Y-%m-%dT%H:%M')
+    duration = end_dt - start_dt
+
+    if duration <= timedelta(0):
+        conn.close()
+        return "⛔ End time must be after start time"
+
+    elif duration > timedelta(days=15):
+        conn.close()
+        return "⛔ Reservation cannot be longer than 15 days"
+
+    if active_count >= max_reservations:
+        conn.close()
+        return "⚠️ You already have 2 active reservations."
+
+    if resource_id:
+        cursor.execute("""
+            INSERT INTO reservations (user_id, resource_id, start_datetime, end_datetime,controller_id)
+            VALUES (%s, %s, %s, %s,%s)
+        """, (user_id, resource_id, start_dt, end_dt, controller_id))
+        notify_user(
+            to_email=session.get('username'),
+            subject="Reservation Confirmed",
+            email_body=f"Your reservation for resource ID {resource['name']} and {controller['name']} from {start_dt} to {end_dt} has been confirmed.",
+        )
+    else:
+        cursor.execute("""
+            INSERT INTO reservations (user_id, start_datetime, end_datetime,controller_id)
+            VALUES (%s, %s, %s, %s)
+        """, (user_id, start_dt, end_dt, controller_id))
+        notify_user(
+            to_email=session.get('username'),
+            subject="Reservation Confirmed",
+            email_body=f"Your reservation for {controller['name']} from {start_dt} to {end_dt} has been confirmed.",
+        )    
+
+    conn.commit()
+    conn.close()
+    return redirect(url_for('reservation.reserve_page', resource_id=resource_id, controller_id=controller_id))
 
 
 @reservation_bp.route('/delete_reservation/<int:id>')
