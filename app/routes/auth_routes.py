76caf51cb6ea423d630fdb97dc173b65a3971820
Silva <Alvita.Silva@commscope.com>
2025-10-23 10:16:09 +0530

    added start and end date constraints

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from app.utils.db import get_db_connection
 from flask import jsonify
+from ldap3 import Server, Connection, ALL, NTLM, SIMPLE
 
 auth_bp = Blueprint('auth', __name__)
+
+# LDAP Configuration
+LDAP_SERVER = 'ldap://your-ad-server.com'
 
 @auth_bp.route('/')
 def index():
@@ -57,6 +61,29 @@ def login():
             return jsonify(status='error',message="❌ Invalid login credentials"),401
     else:
         return redirect(url_for('auth.index'))
+    
+    # LDAP code 
+    # if request.method == 'POST':
+    #     data = request.get_json()
+    #     username = data.get('username')  
+    #     password = data.get('password')
+    #     role = data.get('role')
+
+    #     if not username or not password or not role:
+    #         return jsonify(status='error', message="Missing username, password, or role.")
+
+    #     # Use email directly as the bind user
+    #     server = Server(LDAP_SERVER, get_info=ALL)
+    #     conn = Connection(server, user=username, password=password, authentication=SIMPLE)
+
+    #     if conn.bind():
+    #         session['username'] = username
+    #         session['role'] = role
+    #         return jsonify(status='success', message="Login successful"), 200
+    #     else:
+    #         return jsonify(status='error', message="❌ Invalid login credentials"), 401
+    # else:
+    #     return redirect(url_for('auth.index'))
     
 @auth_bp.route('/user')
 def user_dashboard():
