e870940f9bddba680e13d0beae8073ef930ed037
Silva <Alvita.Silva@commscope.com>
2026-04-09 16:19:06 +0530

    removed unwanted files

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -41,10 +41,6 @@ def is_admin_department(user_department):
 LDAP_SERVER = os.environ.get('LDAP_SERVER')
 SERVICE_ACCOUNT_DN = os.environ.get('SERVICE_ACCOUNT_DN')
 SERVICE_ACCOUNT_PASSWORD = os.environ.get('SERVICE_ACCOUNT_PASSWORD')
- 
-ADMIN_GROUPS = {
-    "#PLM-PMM-TME ALL"
-}
 
 ADMIN_GROUPS = {
     "#PLM-PMM-TME ALL",
@@ -133,34 +129,6 @@ def confirm():
  
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    # if request.method == 'POST':
-    #     data=request.get_json()
-    #     username =data.get('username')
-    #     password =data.get('password')
-    #     role =data.get('role')
- 
-    #     if not username or not password or not role:
-    #         return jsonify(status='error',message="Missing username, password, or role.")
- 
-    #     conn = get_db_connection()
-    #     cursor = conn.cursor(dictionary=True)
-    #     cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s AND role=%s",
-    #                    (username, password, role))
-    #     user = cursor.fetchone()
-    #     conn.close()
- 
-    #     if user:
-    #         session['user_id'] = user['id']    
-    #         session['username'] = username
-    #         session['role'] = role
-    #         return jsonify(status='success',message="Login successful"),200
-    #         # return redirect(url_for('inventory.inventory'))
-    #     else:
-    #         return jsonify(status='error',message="❌ Invalid login credentials"),401
-    # else:
-    #     return redirect(url_for('auth.index'))
-   
- 
     if request.method == 'POST':
         data=request.get_json()
         username =data.get('username')
